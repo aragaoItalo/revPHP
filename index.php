285b5df2e367b1923b68cb216d95c72bb69e3116
aragaoItalo <italoaragao23@gmail.com>
2024-05-23 22:43:30 -0300

    Inclusão script Firebase

--- index.php
+++ index.php
@@ -5,10 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
+    <script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyDJ0BZDl7HIcRlp800n5ZyxCeH-geBxnPw",
+    authDomain: "meufirebaseauth.firebaseapp.com",
+    projectId: "meufirebaseauth",
+    storageBucket: "meufirebaseauth.appspot.com",
+    messagingSenderId: "562663390578",
+    appId: "1:562663390578:web:ecbe7958c9aae1846e71eb"
+  };
+
+  // Initialize Firebase
+  window.fbApp = initializeApp(firebaseConfig);
+  window.fbAuth = getAuth(window.fbApp);
+
+        createUserWithEmailAndPassword(window.fbAuth, "italo.arruda@etec.sp.gov.br", "123456789")
+        .then((userCredential) =>{
+            var user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) =>{
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage);
+        });
+</script>
 </head>
 <body>
     <h1>To do List</h1>
     <?php
          include 'src/main.php'  ?> 
+    <br>
+    <label for="email">Email</label><br>
+    <input type="email" id="email" name="email"><br>
+    <label for="password">Password</label><br>
+    <input type="password" id="password" name="password"><br>
+    <button id="btnLogin">Login</button><button id="btnRegUser" onclick="registrarUsuario()">Registrar Usuario</button>
 </body>
 </html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6d8dbf5a1633857190ef6cd0882c0e5e3d054f26',
+        'name' => 'vendor/phprevisao',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/phprevisao' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6d8dbf5a1633857190ef6cd0882c0e5e3d054f26',
+            'dev_requirement' => false,
+        ),
+    ),
+);
